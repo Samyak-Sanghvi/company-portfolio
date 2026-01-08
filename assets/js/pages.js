368663242979c8ba2bf745aab044ccb870765dc1
Samyak-Sanghvi <samyaksanghvi20@gmail.com>
2026-01-08 21:29:01 +0530

    All changes

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -1,4 +1,4 @@
-function includeHTML() {
+function includeHTML(callback) {
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
@@ -9,13 +9,13 @@ function includeHTML() {
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("w3-include-html");
-          includeHTML();
+          includeHTML(callback);
         }
       }
       xhttp.open("GET", file, true);
@@ -24,4 +24,5 @@ function includeHTML() {
       return;
     }
   }
+  if (callback) callback();
 }
